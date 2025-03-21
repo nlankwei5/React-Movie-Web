becf445008b9288b495151d5bf2ebdca6381be1f
nlankwei5 <lampteyebenezer220@gmail.com>
2025-03-21 13:59:04 +0000

    APi File created

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+const API_KEY = "83bb9f97b536501f70653a1be68122a2";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/movie/popular?api_key= ${API_KEY} `
+  );
+  const data = await response.json();
+  return data.results;
+};
+
+export const getSearchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key= ${API_KEY}&query=${encodeURIComponent(
+      query
+    )} `
+  );
+  const data = await response.json();
+  return data.results;
+};
