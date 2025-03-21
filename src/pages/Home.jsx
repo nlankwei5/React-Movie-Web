ab4d4623341d641c122bf2868948505f7671071c
nlankwei5 <lampteyebenezer220@gmail.com>
2025-03-21 14:38:59 +0000

    Movie card.jsx, Home.jsx and api file updated
    MovieCard updated to show images of movies. Home.jsx modifid to get api to render movies. Api bug fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,32 @@
 import MovieCard from "../components/MovieCard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { searchMovies, getPopularMovies } from "../services/api";
 import "../css/Home.css";
 
 function Home() {
   const [searchQuery, setSearchQuery] = useState("");
+  const [movies, setMovies] = useState([]); 
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-  const movies = [
-    { id: 1, title: "John Wick", release_date: "2022" },
-    { id: 2, title: "Terminator", release_date: "2000" },
-    { id: 3, title: "Commando", release_date: "1997" },
-  ];
 
+    
+  useEffect(() => {
+    const loadPopularMovies = async () =>{
+        try {
+            const popularMovies = await getPopularMovies()
+            setMovies(popularMovies)
+        } catch (err){
+            console.log(err)
+            setError("Failed to load movies.....")
+        }
+        finally{
+            setLoading(false)
+        }
+    }
+
+    loadPopularMovies()
+  }, [])
   const handleSearch = (e) => {
     e.preventDefault();
     alert(searchQuery);
--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
-const API_KEY = "83bb9f97b536501f70653a1be68122a2";
+const API_KEY = "f7311b1928e41b1cffb9f3c8f6682b99";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
   const response = await fetch(
-    `${BASE_URL}/movie/popular?api_key= ${API_KEY} `
+    `${BASE_URL}/movie/popular?api_key=${API_KEY}`
   );
   const data = await response.json();
   return data.results;
@@ -11,7 +11,7 @@ export const getPopularMovies = async () => {
 
 export const searchMovies = async (query) => {
   const response = await fetch(
-    `${BASE_URL}/search/movie?api_key= ${API_KEY}&query=${encodeURIComponent(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
       query
     )} `
   );
